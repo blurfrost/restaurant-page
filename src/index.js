e4f2028c489d749c1c150750fdaccc8bce49cc38
blurfrost <ksyc07@gmail.com>
2026-08-16 16:14:27 +0800

    Add menu page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./styles.css";
 import { updateHome } from "./home";
+import { updateMenu } from "./menu";
 
 updateHome();
 
 const homeButton = document.querySelector("#home");
-
 homeButton.addEventListener("click", updateHome);
+
+const menuButton = document.querySelector("#menu");
+menuButton.addEventListener("click", updateMenu);
